c6677aa08c4f4d038aded9767990f6979bc52b64
joaoeduardodias <joaoeduardodias123@gmail.com>
2019-09-19 17:39:01 -0300

    adicionando rotas de treino, fazendo a controller de programas

--- app/Controllers/Http/ProgramaController.js
+++ app/Controllers/Http/ProgramaController.js
@@ -1,5 +1,5 @@
-'use strict'
-
+'use strict';
+const Programas = use('App/Models/Programa');
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
@@ -8,86 +8,146 @@
  * Resourceful controller for interacting with programas
  */
 class ProgramaController {
-  /**
-   * Show a list of all programas.
-   * GET programas
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
-  }
+	/**
+	 * Show a list of all programas.
+	 * GET programas
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 * @param {View} ctx.view
+	 */
+	async indexganho({}) {
+		const prog = Programas.query()
+			.where(`fk_objetivo`, 1)
+			.fetch();
+		return prog;
+	}
 
-  /**
-   * Render a form to be used for creating a new programa.
-   * GET programas/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
+	async indexperda() {
+		const prog = Programas.query()
+			.where('fk_objetivo', 2)
+			.fetch();
+		return prog;
+	}
 
-  /**
-   * Create/save a new programa.
-   * POST programas
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store ({ request, response }) {
-  }
+	// nivel iniciante
+	async indexganhon1({}) {
+		const prog = Programas.query()
+			.where(`fk_objetivo`, 1)
+			.andWhere(`fk_nivel`, 1)
+			.fetch();
+		return prog;
+	}
 
-  /**
-   * Display a single programa.
-   * GET programas/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
-  }
+	async indexperdan1() {
+		const prog = Programas.query()
+			.where('fk_objetivo', 2)
+			.andWhere(`fk_nivel`, 1)
+			.fetch();
+		return prog;
+	}
 
-  /**
-   * Render a form to update an existing programa.
-   * GET programas/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
+	// nivel intermediario
+	async indexganhon2({}) {
+		const prog = Programas.query()
+			.where(`fk_objetivo`, 1)
+			.andWhere(`fk_nivel`, 2)
+			.fetch();
+		return prog;
+	}
 
-  /**
-   * Update programa details.
-   * PUT or PATCH programas/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-  }
+	async indexperdan2() {
+		const prog = Programas.query()
+			.where('fk_objetivo', 2)
+			.andWhere(`fk_nivel`, 2)
+			.fetch();
+		return prog;
+	}
+	// nivel profissional
 
-  /**
-   * Delete a programa with id.
-   * DELETE programas/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
-  }
+	async indexganhon3({}) {
+		const prog = Programas.query()
+			.where(`fk_objetivo`, 1)
+			.andWhere(`fk_nivel`, 3)
+			.fetch();
+		return prog;
+	}
+
+	async indexperdan3() {
+		const prog = Programas.query()
+			.where('fk_objetivo', 2)
+			.andWhere(`fk_nivel`, 3)
+			.fetch();
+		return prog;
+	}
+
+	/**
+	 * Create/save a new programa.
+	 * POST programas
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 */
+	async store({ request }) {
+		const data = request.only([
+			'descricao',
+			'nome',
+			'fk_objetivo',
+			'fk_nivel',
+		]);
+		const prog = await Programas.create(data);
+		return prog;
+	}
+
+	/**
+	 * Display a single programa.
+	 * GET programas/:id
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 * @param {View} ctx.view
+	 */
+	async show({ params }) {
+		const prog = Programas.finOrFail(params.nome);
+		return prog;
+	}
+
+	/**
+	 * Update programa details.
+	 * PUT or PATCH programas/:id
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 */
+	async update({ params, request }) {
+		const prog = Programas.finOrFail(params.id);
+		const data = request.only([
+			'nome',
+			'descricao',
+			'fk_objetivo',
+			'fk_nivel',
+		]);
+		prog.merge(data);
+		await prog.save();
+		return prog;
+	}
+
+	/**
+	 * Delete a programa with id.
+	 * DELETE programas/:id
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 */
+	async destroy({ params }) {
+		const prog = Programas.finOrFail(params.id);
+		prog.delete();
+	}
 }
 
-module.exports = ProgramaController
+module.exports = ProgramaController;
--- start/routes.js
+++ start/routes.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +14,23 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use('Route')
+const Route = use('Route');
 
-Route.post('/forgot','ForgotPasswordController.store')
-Route.post('/reset','ResetePasswordController.store')
+Route.post('/cadastro', 'AuthController.register');
+Route.post('/login', 'AuthController.authenticate');
+
+Route.post('/forgot', 'ForgotPasswordController.store');
+Route.post('/reset', 'ResetePasswordController.store');
+
+// treinos
+
+Route.get('/treinoganho', 'ProgramaController.indexganho');
+Route.get('/treinoperda', 'ProgramaController.indexperda');
+
+//  treinos por nível
+Route.get('/treinoganhon1', 'ProgramaController.indexganhon1');
+Route.get('/treinoperdan1', 'ProgramaController.indexperdan1');
+Route.get('/treinoganhon2', 'ProgramaController.indexganhon2');
+Route.get('/treinoperdan2', 'ProgramaController.indexperdan2');
+Route.get('/treinoganhon3', 'ProgramaController.indexganhon3');
+Route.get('/treinoperdan3', 'ProgramaController.indexperdan3');
