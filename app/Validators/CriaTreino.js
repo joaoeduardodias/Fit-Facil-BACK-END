283cb98586480a06dc6822b0ca4ec454116efd4e
joaoeduardodias <joaoeduardodias123@gmail.com>
2019-11-18 21:04:37 -0300

    Validacoes e tratamento de erro de treino

--- app/Validators/CriaTreino.js
+++ app/Validators/CriaTreino.js
@@ -4,6 +4,8 @@ class CriaTreino {
 	get rules() {
 		return {
 			nome: [rule('required'), rule('max', '80')],
+			repeticoes: [rule('required')],
+			descanso: [rule('required')],
 			objetivo_id: [rule('required'), rule('exists', ['objetivos', 'id'])],
 			nivel_id: [rule('required'), rule('exists', ['nivel', 'id'])],
 		};
